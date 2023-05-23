6e4a85617c8e6e9b93525c3034840eb1ae48a183
josephe44 <josephe442@gmail.com>
2023-05-23 08:36:26 +0100

    added a slug plugin

--- config/plugins.js
+++ config/plugins.js
@@ -16,4 +16,17 @@ module.exports = ({ env }) => ({
     },
   },
   // ...
+  // ...
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: "slug",
+          references: "title",
+        },
+      },
+    },
+  },
+  // ...
 });
